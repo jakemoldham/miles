bd71a8dd1859e9ceedbbd1c36a78f0c4f01f8545
jakemoldham <jakemoldham@gmail.com>
2017-02-22 22:59:57 -0700

    finishing adjustments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,13 @@ class StaticPagesController < ApplicationController
     Guest.create(coming: params[:coming], name: params[:name], guests: params[:guests])
     redirect_to root_path, notice: "WE ARE GOING TO PARTAY!"
   end
+
+  def coming
+    @guests = Guest.all.where(coming: 1)
+    @not_guests = Guest.all.where(coming: nil)
+    all_guests = Guest.all.where(coming: 1).map{|g| g.guests}
+    @sum_guests = all_guests.sum
+    all_guests = Guest.all.where(coming: nil).map{|g| g.guests}
+    @sum_not_guests = all_guests.sum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
 
+  get "/coming", to: "static_pages#coming", as: :coming
   post "static_pages/guest", to: "static_pages#guest", as: :static_pages_guest
 
   devise_for :users
